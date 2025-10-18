57f8f5507ee8362bb2ed820c96179e9efa2db3fa
ah3pak <ah3pak@gmail.com>
2025-10-19 02:48:48 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 
 app = FastAPI()
 
-@app.get("/healthz")
-def health():
+@app.post("/v1/report")
+async def send_now(req: Request):
+    data = await req.json()
     return {"ok": True}
